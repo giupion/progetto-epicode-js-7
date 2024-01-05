764ac0e97fb2483299f69a446e5ab5e9dc3acced
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-01-05 16:21:39 +0100

    devi fare la roba di capire in che pagina ti trovi, poi fare il post da value del form

--- progetto/assets/js/script-homepage.js
+++ progetto/assets/js/script-homepage.js
@@ -1,5 +1,30 @@
-
+//verifica le funzionalita di ogni pagina e scrivi  quello che si deve fare per ogni pagina
 document.addEventListener('DOMContentLoaded', () => {
+    if (document.querySelector("#FormAddPrd")) {
+        //Setto un listener sul btn del form e richiamo la funzione di validazione
+        document
+          .querySelector("#btn-add")
+          .addEventListener("click", validateFormAdd);
+        //Setto un listener sul btn del form per il reset e richiamo la funzione di reset
+        document
+          .querySelector("#btn-reset-yes")
+          .addEventListener("click", resetFormAdd);
+        //Se è presente il div content-list della pagina modifica prodotto
+      } else if (document.querySelector("#content-list")) {
+        //Richiamo la funzione per creare la lista
+        readProductChange();
+        document
+          .querySelector("#btn-aggiorna")
+          .addEventListener("click", validateFormChange);
+        document
+          .querySelector("#btn-annulla")
+          .addEventListener("click", resetFormChange);
+        //Se non è soddisfatta nessuna delle condizioni sopracitate vuol dire che siamo nella homepage
+      } else {
+        //e che quindi mostra l'elenco
+      
+      }
+      
     fetch('https://striveschool-api.herokuapp.com/api/product/', {
              method: 'GET',
          headers: {
@@ -25,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
                                 <p class="card-text">${e._id}</p>
                                 <p class="card-text">${e.price}€</p>
                                 <button type="button" class="btn btn-primary">Compra ora</button>
-                                <button type="button" class="btn btn-danger">Scarta</button>
+                                <button type="button" class="btn btn-danger id="buttondelete">Scarta</button>
                               </div>
                              </div>
                              </div>`
@@ -65,7 +90,39 @@ document.addEventListener('DOMContentLoaded', () => {
     let p27 = new Product("Metal gear solid 2", "Special ps2", "Sony", "https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.hardcoregaming101.net%2Fwp-content%2Fuploads%2F2023%2F05%2Fmetal-gear-solid-2-86.png&tbnid=7aamaNy8DWybHM&vet=12ahUKEwjawYal-sGDAxV4i_0HHfDcDY4QMygVegQIARB6..i&imgrefurl=http%3A%2F%2Fwww.hardcoregaming101.net%2Fmetal-gear-solid-2%2F&docid=f92H7Oy0NO0haM&w=1280&h=720&q=metal%20gear%20solid%20ps2&safe=active&ved=2ahUKEwjawYal-sGDAxV4i_0HHfDcDY4QMygVegQIARB6", 100);
     console.log(p27);
 
+   function addproduct(){ let prd={
+        name:document.querySelector(' input#nome-prodotto').value.trim(),
+        description: document.querySelector("#product-description").value.trim(),
+       brand:  document.querySelector(' input#marchio').value.trim(),
+        imageUrl: document.querySelector(' input#url-immagine').value.trim(),
+        price: document.querySelector(' input#prezzo-prodotto').value.trim(),
+        
+    
+    
+    
+    };
+    sendproduct(prd)
+}
 
+    
+    
+   function sendproduct(prd){fetch('https://striveschool-api.herokuapp.com/api/product/', {
+        method: "POST",
+        body: JSON.stringify(addproduct(prd)),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            Authorization: ' Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4M2YyOWMwNTgzNTAwMTg1MjMxODMiLCJpYXQiOjE3MDQ0NjI4MDksImV4cCI6MTcwNTY3MjQwOX0.FntuC1oARFezqS7gJHYK8dBuKeV_K9QaXtZC6DOjSdo'
+        }
+    })
+        .then(response => response.json())
+        .then(json => console.log(json))
+    
+        .catch(error => console.log(error))}
+    
+    //manca la roba di decidere dei bottoni e capire in che pagina siamo
+    
+    
+    
 
 
 
@@ -88,7 +145,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(error => console.log(error))
 
 
-    fetch('https://striveschool-api.herokuapp.com/api/product/' + '6578917526761400183c34f1', {
+       fetch('https://striveschool-api.herokuapp.com/api/product/' + '6578917526761400183c34f1', {
         method: 'DELETE',
 
         headers: {
@@ -102,7 +159,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
         .catch(error => console.log(error))
 })
-
 
 
 
